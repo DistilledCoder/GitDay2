d25925712aa09b37f2f7740df929cb6c73f74e98
ericw <ericw@UncleRicco.san.rr.com>
2025-01-31 15:18:22 -0800

    added simple addition

--- src/day2/DemoDay2.java
+++ src/day2/DemoDay2.java
@@ -4,6 +4,9 @@ public class DemoDay2 {
 	
 	public static void main(String[] args) {
 		System.out.println("Eric is learning new things");
+		int a = 10;
+		int b = 20;
+		System.out.println(a + b);
 	}
 
 }
